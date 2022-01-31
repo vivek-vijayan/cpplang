46467a599d53a3e28257507a03a8e36546705fa5
Vivek Vijayan (@Official) <vivekvijayan3@icloud.com>
2022-02-01 02:30:01 +0530

    Updated Binary Tree

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,41 +1,75 @@
-#include<iostream>
+#include <iostream>
+#include<queue>
 
-struct Node 
+struct Node
 {
 	int data;
-	struct Node* left;
-	struct Node* right;
+	struct Node *left;
+	struct Node *right;
 
-	Node(int val) {data = val;}
+	Node(int val) { data = val; }
 };
 
-Node* CreateNode(int data)
+Node *CreateNode(int data)
 {
 	Node *root = new Node(data);
-	root -> left = NULL;
-	root -> right = NULL;
+	root->left = NULL;
+	root->right = NULL;
 	return root;
 }
 
-void inorder(Node* root)
+Node *DeleteNode(Node *nodename)
 {
-	if (root == NULL) return;
-	inorder(root -> left);
-	std::cout << "Data : " << root -> data << std::endl;
-	inorder(root -> right);
+	if (nodename->left == NULL && nodename->right == NULL)
+	{
+		nodename->data = 0;
+		std::cout << " Node deleted successfully";
+		return NULL;
+	}
+	else
+		std::cout << "Cannot remove the parent member .." << std::endl;
+		return nodename;
+}
+
+void inorder(Node *root)
+{
+	if (root == NULL)
+		return;
+	inorder(root->left);
+	std::cout << "Data : " << root->data << std::endl;
+	inorder(root->right);
 }
 
 int main()
 {
+	bool check = true;
+	char answer = 'y';
+	int data;
 	struct Node *root = new Node(10);
-	root -> left = CreateNode(20);
-	root -> right = CreateNode(30);
-	root -> left -> left = CreateNode(40);
-	root -> right -> right = CreateNode(50);
-
-	inorder(root);
-	
-	return 0;
-}	
-
-
+	std::queue<Node *> q;
+	q.push(root);
+	while(check)
+	{
+		std::cout << "Do you want to add a node?    :"; std::cin >> answer; if (answer == 'y'){
+			std::cout << "Do you want left or right?     :"; std::cin >> answer; if(answer == 'l'){
+				std::cout << "Enter the data: " ; std::cin >> data;
+				root -> left = CreateNode(data);
+				q.push(root -> left);
+			}
+			else
+			{
+				std::cout << "Enter the data: " ; std::cin >> data;
+				root -> right = CreateNode(data);
+				q.push(root -> right);
+			}
+		}
+		std::cout << "Do you want to continue? "; std::cin >> answer; if (answer == 'n') check = false;
+	}
+	std::cout << " Data structure:" << std::endl;
+	for (int i = 0; i < q.size() + 1; i++)
+	{
+		Node* tempNode = q.front();
+		std::cout << tempNode -> left -> data << " <- " << tempNode -> data << " -> " << tempNode -> right -> data << std::endl;
+		q.pop();
+	}
+}
